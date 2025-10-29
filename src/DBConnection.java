196c542237e8e4ac1ecbbcd24bfb746c80efda74
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-29 11:29:12 -0500

    commit - DBConnection .

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,2 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL = "jdbc:oracle:thin:@//localhost:1521/orcl";
+    private static final String USER = "system";
+    private static final String PASS = "Tapiero123";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
 }
